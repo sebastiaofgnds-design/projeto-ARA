6ca17c8b18f9e854a4684101c92f2049bf91deae
sebastiaofgnds-design <sebastiaofgnds@gmail.com>
2025-10-21 10:33:13 -0300

    Create supabase-config.js

---
+++ js/supabase-config.js
@@ -0,0 +1,4 @@
+// Aqui você deve colocar sua URL e ANON KEY protegida com RLS ativa
+const SUPABASE_URL = 'https://SEU_PROJETO.supabase.co';
+const SUPABASE_ANON_KEY = 'SUA_ANON_KEY_AQUI';
+// const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
