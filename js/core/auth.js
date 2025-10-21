f3db64f7a2182881ec5af192f038c48cace4cb46
sebastiaofgnds-design <sebastiaofgnds@gmail.com>
2025-10-21 16:57:09 -0300

    Create auth.js

---
+++ js/core/auth.js
@@ -0,0 +1,14 @@
+import { getTenantFromPath } from './tenancy.js';
+import { fakeLogin } from '../services/supabase.js';
+
+export async function login(email, senha) {
+  const user = await fakeLogin(email, senha);
+  const tenant = getTenantFromPath();
+
+  if (!user || user.tenant !== tenant) {
+    return false;
+  }
+
+  localStorage.setItem("user", JSON.stringify(user));
+  return true;
+}
